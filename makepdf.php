81754c99e2905c31e1b1aad89bf3020e02efbc1b
Tariq <selawi.be@gmail.com>
2021-08-03 11:56:47 +0200

    heroku

--- makepdf.php
+++ makepdf.php
@@ -46,7 +46,7 @@ $data = '
      </div>
         <!-- ########################## logo page  ########################## -->
             <div>
-            <img src="/images/04.png" style="width:100%;" alt="SVI">
+            <img src="/Applications/XAMPP/xamppfiles/htdocs/pdfapp/images/04.png" style="width:100%;" alt="SVI">
             </div>
         <!-- ########################## second page ########################## -->
             <div class="cover-bg">
